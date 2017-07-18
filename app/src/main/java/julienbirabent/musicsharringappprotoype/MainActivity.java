428212883f15e10a5cb2e2c58bafcb1e4243d3de
Julien <jul.birabent@gmail.com>
2017-07-17 23:49:05 -0400

    Ajout de l'action bar au top

--- app/src/main/java/julienbirabent/musicsharringappprotoype/MainActivity.java
+++ app/src/main/java/julienbirabent/musicsharringappprotoype/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected Toolbar toolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,10 +19,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.generated_playlist:
+                    toolbar.setTitle(R.string.title_generated_playlist);
                 return true;
                 case R.id.custom_playlist:
+                    toolbar.setTitle(R.string.title_custom_playlist);
                     return true;
                 case R.id.my_profile:
+                    toolbar.setTitle(R.string.title_profile);
                     return true;
             }
             return false;
@@ -33,8 +38,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* init the toolbar*/
+        toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /* Init the bottom nav bar*/
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
     }
 
 }

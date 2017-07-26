62eb2953479363e6c0e06a887c07f29aa8e9f498
Julien <jul.birabent@gmail.com>
2017-07-26 12:51:31 -0400

    Ajout du modèle Comment

---
+++ app/src/main/java/julienbirabent/musicsharringappprotoype/models/Comment.java
@@ -0,0 +1,32 @@
+package julienbirabent.musicsharringappprotoype.models;
+
+/**
+ * Created by julbi on 2017-07-26.
+ */
+
+public class Comment {
+
+    private UserProfile commentOwner;
+    private String body;
+
+    public Comment(UserProfile commentOwner, String body) {
+        this.commentOwner = commentOwner;
+        this.body = body;
+    }
+
+    public UserProfile getCommentOwner() {
+        return commentOwner;
+    }
+
+    public void setCommentOwner(UserProfile commentOwner) {
+        this.commentOwner = commentOwner;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
